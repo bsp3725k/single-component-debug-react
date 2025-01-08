408c463afdfef753ea6af7ddfcb8ede08c5d6243
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2025-01-08 17:58:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
+// Render the App component on the DOM node with id="root"
+ReactDOM.render(<App />, document.getElementById("root"));
 
