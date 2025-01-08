7c6b1206ef6ee787316ab99d503220df892319ea
bsp3725k <bhavanisivaprasad39@gmail.com>
2025-01-08 16:55:36 +0530

    Your commit message

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ const App = () => {
       <li>React Functional Components</li>
       <li>JSX Syntax</li>
       <li>DOM Manipulation using ReactDOM</li>
+      <li>Done!</li>
     </ul>
   </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
 // Render the App component on the DOM node with id="root"
 ReactDOM.render(<App />, document.getElementById("root"));
-
