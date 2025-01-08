12d6dfdc3e8307137c6f97e62b872e784a50eb45
bsp3725k <bhavanisivaprasad39@gmail.com>
2025-01-08 16:37:08 +0530

    Updated App.js, index.html, and index.js with necessary changes

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
+// Render the App component on the DOM node with id="root"
+ReactDOM.render(<App />, document.getElementById("root"));
 
